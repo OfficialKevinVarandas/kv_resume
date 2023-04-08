a6223308438638ad51884d981f1b4a6168033327
Kevin Varandas <kevin.varandas@gmail.com>
2023-04-09 01:18:14 +0200

    update toggle theme of the app

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+// import { NgModule } from '@angular/core';
+
+// import { ToggleThemeModule } from 'projects/toggle-theme/src/lib/toggle-theme.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [AppComponent],
+//   imports: [ToggleThemeModule],
+//   providers: [],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-import { SwitchThemeComponent } from '../switch-theme/switch-theme.component';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ToggleThemeComponent, ToggleThemeServivce } from '@kv/toggle-theme';
+import { Subscription } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [SwitchThemeComponent],
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  imports: [ToggleThemeComponent],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  private _subscription: Subscription | undefined;
+
+  constructor(private toggleService: ToggleThemeServivce) {}
+
+  ngOnInit(): void {
+    this._subscription = this.toggleService.isDark.subscribe((dark) =>
+      document.documentElement.classList.toggle('dark')
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._subscription?.unsubscribe();
+  }
+}
